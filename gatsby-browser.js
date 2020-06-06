e0a22c282d2a0c841d5a8329166cb01f4dc288ee
RocketLL <luc4sl33@gmail.com>
2020-06-07 01:50:32 +0900

    eliminate fout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,3 @@
 import Wrapper from "./wrapPageElement"
-import { getDark, setDark } from "./src/components/initial"
 
 export const wrapPageElement = Wrapper
-
-export const onClientEntry = () => {
-  setDark(getDark())
-}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,13 @@
 import React from "react"
 import Layout from "./wrapPageElement"
 
-import Initial from "./src/components/initial"
-
 export const wrapPageElement = Layout
+const code = `!function(){var t,e;e=localStorage.getItem("dark"),t="true"===e,document.body.setAttribute("theme",t?"dark":"light"),localStorage.setItem("dark",t?"true":"false")}();`
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents(
+    <script
+      dangerouslySetInnerHTML={{ __html: code }}
+    />
+  )
+}
