4fd946b9ad04641002db21dff92d6aaddcc03161
RocketLL <luc4sl33@gmail.com>
2020-06-07 03:18:26 +0900

    edit story

--- src/components/story.jsx
+++ src/components/story.jsx
@@ -15,25 +15,29 @@ const Story = () => {
         <p className={styles.story}>
           <span className={styles.emph}>Hi
           <Emoji className={styles.emoji} type="wave" />, I'm Lucas.</span>
-           I'm a student from South Korea attending <a href="/" className={link}>Hana Academy Seoul</a>. I enjoy physics
-          and programming.
+          I'm a student from South Korea attending <a href="https://hana.hs.kr" className={link}>Hana Academy Seoul</a>.
+          I enjoy physics and programming.
         </p>
         <p className={styles.story}>
-          My areas of interest include Scientific Computation and Web
-          Development. I am proficient at Python, Go, JavaScript, MATLAB, and
-          more. I am comfortable with modern web technologies including React,
-          TypeScript, and GatsbyJS(powering this site!).
+          I wrangle code and equations every day.
+          I love using modern tools to solve my problems, both in physics and programming.
+          I am proficient at Python, Go, JS/TS
         </p>
       </>
     )
   else
     return (
-      <p className={`${styles.story} kr`}>
-        안녕하세요
-        <Emoji className={styles.emoji} type="wave" />, 이윤규입니다. 저는
-        하나고등학교에 재학중인 학생입니다. 저는 물리학과 프로그래밍에 관심이
-        많습니다.
-      </p>
+      <>
+        <p className={`${styles.story} kr`}>
+          <span className={styles.emph}>안녕하세요
+          <Emoji className={styles.emoji} type="wave" />, 이윤규입니다.</span>
+          하나고등학교에 재학중인 학생입니다.
+          저는 물리학과 프로그래밍에 관심이 많습니다.
+        </p>
+        <p className={`${styles.story} kr`}>
+          JavaScript, TypeScript
+        </p>
+      </>
     )
 }
 
