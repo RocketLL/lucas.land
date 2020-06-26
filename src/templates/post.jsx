f80844b9646bba947940146f38c7f008432760b1
RocketLL <luc4sl33@gmail.com>
2020-06-26 11:52:57 +0900

    update post template

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -5,6 +5,7 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
 import PostHeader from "../components/postHeader"
 import styles from "../scss/post.module.scss"
+import { typography } from "../scss/text.module.scss"
 
 import "../scss/syntax.global.scss"
 import "katex/dist/katex.min.css"
@@ -12,12 +13,14 @@ import "katex/dist/katex.min.css"
 const shortcodes = { Link }
 
 const PostTemplate = ({ data: { mdx } }) => (
-  <div className={styles.post}>
+  <div className={`${styles.post} ${mdx.fields.locale}`}>
     <PostHeader />
-    <h1 className={styles.title}>{mdx.frontmatter.title}</h1>
-    <MDXProvider components={shortcodes}>
-      <MDXRenderer>{mdx.body}</MDXRenderer>
-    </MDXProvider>
+    <div className={typography}>
+      <h1 className={styles.title}>{mdx.frontmatter.title}</h1>
+      <MDXProvider components={shortcodes}>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </MDXProvider>
+    </div>
   </div>
 )
 
@@ -28,6 +31,9 @@ export const pageQuery = graphql`
       body
       frontmatter {
         title
+      }
+      fields {
+        locale
       }
     }
   }
