6e42713a9d277072f71cd6f29dd05c2a5f80dbcb
RocketLL <luc4sl33@gmail.com>
2020-08-15 14:10:43 +0900

    clean code

--- content/pages/index.kr.jsx
+++ content/pages/index.kr.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 
 import Bio from "../../src/components/bio"
-import Vis from "./_vis"
 
 import styles from "../../src/scss/layout.module.scss"
 
 const Index = () => (
   <div className={`${styles.layout} ${styles.large}`}>
     <Bio />
-    <Vis />
   </div>
 )
 
