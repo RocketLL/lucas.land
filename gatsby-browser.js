0ad70669bc4902096dd56c5800162e57d705564f
RocketLL <luc4sl33@gmail.com>
2020-08-30 01:36:30 +0900

    ie11 support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
 import Wrapper from "./wrapPageElement"
 
 export const wrapPageElement = Wrapper
+
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`);
+    console.log("IntersectionObserver is polyfilled")
+  }
+}
